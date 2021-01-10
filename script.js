f8608530cdc341e17fcab3ced3432cf78eb57d28
AditiyaS-cloud <71305712+AditiyaS-cloud@users.noreply.github.com>
2021-01-10 18:23:14 +0530

    HighScore logic implemented

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
-
+let highscore = 0;
 let score = 20;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     // 🙄When guess is wrong
@@ -15,6 +15,10 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
 
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
         // ⚡When guess is too high  
     } else if (guess > secretNumber) {
         if (score > 1) {
@@ -38,3 +42,15 @@ document.querySelector('.check').addEventListener('click', function () {
         }
     }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.guess').value = '';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('body').style.backgroundColor = '#222';
+
+})
